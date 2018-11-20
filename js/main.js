03c2325e04eddb88ff24c22ec2e19e0731803bcc
krdowns <krjdowns16@gmail.com>
2018-11-20 14:30:19 -0800

    updated with working carousel and additional formatting to about section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
+$(document).ready(function () {
+    // Activate Carousel
+    $(".carousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+        $(".carousel").carousel(0);
+    });
+    $(".item2").click(function () {
+        $(".carousel").carousel(1);
+    });
+    $(".item3").click(function () {
+        $(".carousel").carousel(2);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $(".carousel").carousel("prev");
+    });
+    $(".right").click(function () {
+        $(".carousel").carousel("next");
+    });
+});
+
 function openNav() {
-    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("mySideNav").style.width = "15%";
 }
 
 function closeNav() {
     document.getElementById("mySideNav").style.width = "0";
+    document.getElementsByClassName(".carousel").style.width ="100%";
 }
+
