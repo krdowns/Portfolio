9b30012b0ff63acef39497dc9a304988e0d6a08f
krdowns <krjdowns16@gmail.com>
2018-11-24 15:58:36 -0800

    added lightbox

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     // Activate Carousel
     $(".carousel").carousel();
 
+    $("#test-carousel").carousel();
+
     // Enable Carousel Indicators
     $(".item1").click(function () {
         $(".carousel").carousel(0);
@@ -20,22 +22,55 @@ $(document).ready(function () {
     $(".right").click(function () {
         $(".carousel").carousel("next");
     });
+    
 });
 
-//-----TESTAMONIALS-----//
-
-$(document).ready(function() {
-    //carousel options
-    $('#quote-carousel').carousel({
-      pause: true, interval: 10000,
-    });
-  });
+// Open the Modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
 
 //-----OPEN AND CLOSE NAV-----//
-function openNav() {
-    document.getElementById("mySideNav").style.width = "15%";
-}
+// function openNav() {
+//     document.getElementById("mySideNav").classList.add("open")
+// }
 
-function closeNav() {
-    document.getElementById("mySideNav").style.width = "0";
-}
+// function closeNav() {
+//     document.getElementById("mySideNav").classList.add("close")
+// }
