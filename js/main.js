2931d1d622d1e8b4042ea95a5dd3b89e641993e5
krdowns <krjdowns16@gmail.com>
2019-02-12 19:21:51 -0800

    updated portfolio

--- js/main.js
+++ js/main.js
@@ -1,77 +1,77 @@
-$(document).ready(function () {
-    // Activate Carousel
-    $(".carousel").carousel();
+// $(document).ready(function () {
+//     // Activate Carousel
+//     $(".carousel").carousel();
 
-    $("#test-carousel").carousel();
+//     $("#test-carousel").carousel();
 
-    // Enable Carousel Indicators
-    $(".item1").click(function () {
-        $(".carousel").carousel(0);
-    });
-    $(".item2").click(function () {
-        $(".carousel").carousel(1);
-    });
-    $(".item3").click(function () {
-        $(".carousel").carousel(2);
-    });
+//     // Enable Carousel Indicators
+//     $(".item1").click(function () {
+//         $(".carousel").carousel(0);
+//     });
+//     $(".item2").click(function () {
+//         $(".carousel").carousel(1);
+//     });
+//     $(".item3").click(function () {
+//         $(".carousel").carousel(2);
+//     });
 
-    // Enable Carousel Controls
-    $(".left").click(function () {
-        $(".carousel").carousel("prev");
-    });
-    $(".right").click(function () {
-        $(".carousel").carousel("next");
-    });
+//     // Enable Carousel Controls
+//     $(".left").click(function () {
+//         $(".carousel").carousel("prev");
+//     });
+//     $(".right").click(function () {
+//         $(".carousel").carousel("next");
+//     });
     
-});
+// });
 
-// OPEN THE LIGHTBOX
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-  }
+// // OPEN THE LIGHTBOX
+// function openModal() {
+//     document.getElementById('myModal').style.display = "block";
+//   }
   
-  // CLOSE THE LIGHTBOX
-  function closeModal() {
-    document.getElementById('myModal').style.display = "none";
-  }
+//   // CLOSE THE LIGHTBOX
+//   function closeModal() {
+//     document.getElementById('myModal').style.display = "none";
+//   }
   
-  var slideIndex = 1;
-  showSlides(slideIndex);
+//   var slideIndex = 1;
+//   showSlides(slideIndex);
   
-  // NEXT/PREVIOUS SLIDES
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//   // NEXT/PREVIOUS SLIDES
+//   function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("demo");
+//     var captionText = document.getElementById("caption");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+//     captionText.innerHTML = dots[slideIndex-1].alt;
+//   }
 
-  // From W3 Schools //
+//   // From W3 Schools //
 
-//-----OPEN AND CLOSE NAV-----//
-// function openNav() {
-//     document.getElementById("mySideNav").classList.add("open")
-// }
+// //-----OPEN AND CLOSE NAV-----//
+// // function openNav() {
+// //     document.getElementById("mySideNav").classList.add("open")
+// // }
 
-// function closeNav() {
-//     document.getElementById("mySideNav").classList.add("close")
-// }
+// // function closeNav() {
+// //     document.getElementById("mySideNav").classList.add("close")
+// // }
